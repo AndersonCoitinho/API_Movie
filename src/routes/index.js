8746e4fc972aed56d385190b530a41925f7e7649
Anderson Coitinho <104800828+AndersonCoitinho@users.noreply.github.com>
2023-06-26 10:58:27 -0300

    Rotas de autenticação

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,11 @@ const routes = Router();
 const usersRoutes = require("./User.Routes");
 const notesRoutes = require("./Notes.Routes");
 const tagsRoutes = require("./Tags.Routes");
+const sessionsRoutes = require("./sessions.routes")
 
 routes.use("/users", usersRoutes)
 routes.use("/notes", notesRoutes)
 routes.use("/tags", tagsRoutes)
-
+routes.use("/sessions", sessionsRoutes)
 
 module.exports = routes;
