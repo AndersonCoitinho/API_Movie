bcf9ce1c1b309745c5d90ae05a89e01e5e8726a4
Anderson Coitinho <104800828+AndersonCoitinho@users.noreply.github.com>
2023-06-26 11:12:47 -0300

    validando email e senha do usuario

--- src/controllers/SessionControllers.js
+++ src/controllers/SessionControllers.js
@@ -1,10 +1,23 @@
-const { response } = require("express");
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+const { compare } = require("bcryptjs")
 
 class SessionsController {
-  async create(request, response){
-    const { email, password } = request.body;
+  async create(req, res) {
+    const { email, password } = req.body;
 
-    return response.json({email, password})
+    const user = await knex("users").where({ email }).first()
+    if (!user) {
+      throw new AppError("E-mail e/ou senha incorreta", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+    if (!passwordMatched) {
+      throw new AppError("Email e/ou senha incorreta", 401)
+    }
+
+    return res.json(user)
   }
 }
+
 module.exports = SessionsController;
